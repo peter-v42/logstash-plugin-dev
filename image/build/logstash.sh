2f436ad92b0095b1d803302494f3c01638b926cb
Peter Hoogendijk <peter@v42.net>
2024-05-28 11:24:59 +0200

    Add all example Java plugins

--- image/build/logstash.sh
+++ image/build/logstash.sh
@@ -10,16 +10,25 @@ cp /build/logstash/versions.yml /usr/share/logstash/
 rm -rf /build/logstash
 
 cd /build
-git clone https://github.com/logstash-plugins/logstash-input-java_input_example.git 
-cd /build/logstash-input-java_input_example/
+git clone https://github.com/logstash-plugins/logstash-codec-java_codec_example.git
+git clone https://github.com/logstash-plugins/logstash-filter-java_filter_example.git
+git clone https://github.com/logstash-plugins/logstash-input-java_input_example.git
+git clone https://github.com/logstash-plugins/logstash-output-java_output_example.git
+rm -rf */.git */.gitignore */README.md */gradlew.bat
+
+mkdir /build/test
+cd /build/test
+git clone https://github.com/logstash-plugins/logstash-input-java_input_example.git
+rm -rf logstash-input-java_input_example/.git
+cd /build/test/logstash-input-java_input_example/
 echo "LOGSTASH_CORE_PATH=/usr/share/logstash/logstash-core" >gradle.properties
 ./gradlew gem
 
-logstash-plugin install --no-verify --local /build/logstash-input-java_input_example/logstash-input-java_input_example-1.0.3.gem
+logstash-plugin install --no-verify --local /build/test/logstash-input-java_input_example/logstash-input-java_input_example-1.0.3.gem
 echo "input {
   java_input_example {}
 }
 output {
   stdout { codec => rubydebug }
 }" >java_input.conf
-logstash -f /build/logstash-input-java_input_example/java_input.conf
+logstash -f /build/test/logstash-input-java_input_example/java_input.conf
